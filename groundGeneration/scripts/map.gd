0294ae061b7a46b108c0e837ae4190bec411f3dd
alex <servalex1@gmail.com>
2021-04-24 16:03:46 +0300

    added basic generation

--- groundGeneration/scripts/map.gd
+++ groundGeneration/scripts/map.gd
@@ -5,15 +5,38 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+const cavernMinWidth = 3
+const cavernMaxWidth = 12
+const cavernMinHeight = 1
+const cavernMaxHeight = 4
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
 	# generate map here
+	var screenWidth := 26
+
 	$TileMap.set_cell(1, 1, 1)
 	$TileMap.set_cell(0, 0, 2)
-
-
+	
+	var width = 0
+	var height = 0
+	var currentDepth = -1
+	
+	var i := -1
+	while currentDepth > -100:
+		width = (randi() % (cavernMaxWidth - cavernMinWidth) + cavernMinWidth)
+		height = (randi() % (cavernMaxHeight - cavernMinHeight) + cavernMinHeight)
+		var cavernStart = (randi() % (screenWidth - width - 1))
+		
+		# set tiles
+		for x in range(0, screenWidth):
+			for y in range(currentDepth, currentDepth-height):
+				if(x > cavernStart and x < cavernStart + width):
+					$TileMap.set_cell(x, y, 1)
+				else:
+					$TileMap.set_cell(x, y, 2)
+		
+		currentDepth = currentDepth - height - 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
