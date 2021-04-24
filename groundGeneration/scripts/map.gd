fa453b5067f39070d97fdd49e5a63d9ce4cfcb29
alex <servalex1@gmail.com>
2021-04-24 17:03:19 +0300

    resized base window, map now may be dinamically loaded

--- groundGeneration/scripts/map.gd
+++ groundGeneration/scripts/map.gd
@@ -10,20 +10,20 @@ const cavernMaxWidth = 20
 const cavernMinHeight = 1
 const cavernMaxHeight = 5
 const generationDepth = 1000
+const screenWidth = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# generate map here
-	var screenWidth := 50
+	generateTiles(10, generationDepth)
 
-#	$TileMap.set_cell(1, 1, 1)
-#	$TileMap.set_cell(0, 0, 2)
-	
+func generateTiles(firstLine, lastLine):
+		# generate map here
+
 	var width = 0
 	var height = 0
-	var currentDepth = 10
+	var currentDepth = firstLine
 	
-	while currentDepth < generationDepth:
+	while currentDepth < lastLine:
 		width = (randi() % (cavernMaxWidth - cavernMinWidth) + cavernMinWidth)
 		height = (randi() % (cavernMaxHeight - cavernMinHeight) + cavernMinHeight)
 		var cavernStart = (randi() % (screenWidth - width - 1))
