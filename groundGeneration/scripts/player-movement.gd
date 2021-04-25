60355acab652676bda3b6d755c67484745ed91f4
Oleksandr Kryvonos <geraclion@gmail.com>
2021-04-25 20:51:36 +0300

    mini tutorial

--- groundGeneration/scripts/player-movement.gd
+++ groundGeneration/scripts/player-movement.gd
@@ -4,7 +4,7 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+export var enableMovement = true
 var speed = 1.0
 
 var target_for_target = Vector2(1,0)
@@ -32,8 +32,6 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_down"):
 		direction.y += 1
 	
-	direction = direction.normalized()
-	
-	
-	move_and_collide(direction * speed)
-	pass
+	if enableMovement:
+		direction = direction.normalized()
+		move_and_collide(direction * speed)
