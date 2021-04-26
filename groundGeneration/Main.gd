f8d90fae5993cdd4e4eca2f53c9d182233779ca8
dmytroz-lohika-tix <dmytroz@tradeix.com>
2021-04-27 02:30:49 +0300

    reset

--- groundGeneration/Main.gd
+++ groundGeneration/Main.gd
@@ -9,12 +9,19 @@ func _pause():
 	if get_tree().paused:
 		$"Menu/Control-Menu".hide()
 		$"Menu/background".hide()
+		$"Menu/Button-Restart".hide()
 		get_tree().paused = false
 	else:
 		$"Menu/Control-Menu".show()
 		$"Menu/background".show()
+		$"Menu/Button-Restart".show()
 		get_tree().paused = true
 
 
 func _on_ButtonPause_pressed():
 	_pause()
+
+
+func _on_ButtonRestart_pressed():
+	get_tree().reload_current_scene()
+	get_tree().paused = false
