b9038558800821d9785de81f26a4ca95f9643fa5
Gleb Sviripa <gleb.sviripa@gmail.com>
2021-05-01 04:47:34 +0300

    update mobile ui

--- groundGeneration/Main.gd
+++ groundGeneration/Main.gd
@@ -1,5 +1,10 @@
 extends Node
 
+func _ready():
+	if OS.has_touchscreen_ui_hint():
+		show_mobile_controls(true)
+	else:
+		show_mobile_controls(false)
 
 func _input(ev):
 	if Input.is_key_pressed(KEY_ESCAPE):
@@ -25,3 +30,53 @@ func _on_ButtonPause_pressed():
 func _on_ButtonRestart_pressed():
 	get_tree().reload_current_scene()
 	get_tree().paused = false
+	
+func show_mobile_controls(isShow: bool):
+	if isShow:
+		$Menu/MobileControls.show()
+	else:
+		$Menu/MobileControls.hide()
+	
+func simulate_key(action, is_pressed):
+	if is_pressed:
+		Input.action_press(action)
+	else:
+		Input.action_release(action)
+
+func _on_UpButton_button_down():
+	simulate_key("ui_up", true)
+
+func _on_UpButton_button_up():
+	simulate_key("ui_up", false)
+
+
+func _on_DownButton_button_down():
+	simulate_key("ui_down", true)
+
+
+func _on_DownButton_button_up():
+	simulate_key("ui_down", false)
+
+
+func _on_LeftButton_button_down():
+	simulate_key("ui_left", true)
+
+
+func _on_LeftButton_button_up():
+	simulate_key("ui_left", false)
+
+
+func _on_RightButton_button_down():
+	simulate_key("ui_right", true)
+
+
+func _on_RightButton_button_up():
+	simulate_key("ui_right", false)
+
+
+func _on_MushroomButton_pressed():
+	simulate_key("ui_mushroom", true)
+
+
+func _on_CampfireButton_pressed():
+	simulate_key("ui_campfire", true)
